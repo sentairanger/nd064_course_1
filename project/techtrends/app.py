3498274b313b776359519e1ce76e35db6dde0598
sentairanger <ed2point0@gmail.com>
2021-08-17 15:29:00 -0600

    Update app.py

--- project/techtrends/app.py
+++ project/techtrends/app.py
@@ -2,12 +2,17 @@ import sqlite3
 
 from flask import Flask, jsonify, json, render_template, request, url_for, redirect, flash
 from werkzeug.exceptions import abort
+import logging
+
+counter = 0
 
 # Function to get a database connection.
 # This function connects to database with the name `database.db`
 def get_db_connection():
+    global counter
     connection = sqlite3.connect('database.db')
     connection.row_factory = sqlite3.Row
+    counter += 1
     return connection
 
 # Function to get a post using its ID
@@ -21,6 +26,43 @@ def get_post(post_id):
 # Define the Flask application
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your secret key'
+
+# Health check
+@app.route('/healthz')
+def status():
+    response = app.response_class(
+        response=json.dumps({"result":"OK - healthy"}),
+        status=200,
+        mimetype='application/json'
+        )
+    app.logger.info('Status request successfull')
+    return response
+
+#Metrics function
+@app.route('/metrics')
+def metrics():
+    connection = get_db_connection()
+    posts =  connection.execute('SELECT * FROM posts').fetchall()
+    connection.close()
+    response = app.response_class(
+        response=json.dumps({"status":"success", "code":0,"data":{"db_connection_count": counter, "posts": len(posts)}}),
+        status=200,
+        mimetype='application/json')
+    app.logger.info('Metrics request successfull')
+    return response
+
+#Gracefully shutdown the app without using CTRL+C
+def shutdown():
+    func = request.environ.get('werkzeug.server.shutdown')
+    if func is None:
+        raise RuntimeError('Not running with the Werkzeug server')
+    func()
+
+# gracefully shutdown the app
+@app.route('/shutdown', methods=['GET'])
+def shutdown_server():
+    shutdown()
+    return 'Server shutting down'
 
 # Define the main route of the web application 
 @app.route('/')
@@ -67,4 +109,5 @@ def create():
 
 # start the application on port 3111
 if __name__ == "__main__":
-   app.run(host='0.0.0.0', port='3111')
+    logging.basicConfig(filename='app.log', level=logging.DEBUG)
+    app.run(host='0.0.0.0', port='3111')
